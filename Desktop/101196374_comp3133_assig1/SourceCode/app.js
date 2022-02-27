4bcab82c20a9d340787d2ce5bc47afe00d6a640f
Vraj <sonivraj848@gmail.com>
2022-02-26 21:39:11 -0500

    Assignment is Done

---
+++ Desktop/101196374_comp3133_assig1/SourceCode/app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/auth');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(isAuth);
+
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://KaranSharma1:KaranSharma1@cluster0.3yyoy.mongodb.net/101196374_comp3133_assig1?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
